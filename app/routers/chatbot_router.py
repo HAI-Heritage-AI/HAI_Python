0280d9e5b22b69e19e3fe42d2764661832fb03e8
AnnSungHee <125858475+AnnSungHee@users.noreply.github.com>
2024-11-13 16:44:21 +0900

    update: [안성희] chatbot 코드 프론트와 연결

--- app/routers/chatbot_router.py
+++ app/routers/chatbot_router.py
@@ -1,10 +1,12 @@
 # app/routers/chatbot_router.py
-from fastapi import APIRouter
+from fastapi import APIRouter, Request
 from app.chatbot.chat_with_faiss import process_chat
 
 chatbot_router = APIRouter()
 
 @chatbot_router.post("/chatbot")
-async def chatbot_endpoint(input_text: str):
+async def chatbot_endpoint(request: Request):
+    data = await request.json()  # JSON 데이터 파싱
+    input_text = data.get("input_text")
     response = process_chat(input_text)
     return {"response": response}
--- main.py
+++ main.py
@@ -1,16 +1,22 @@
 # main.py
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
 from app.routers.chatbot_router import chatbot_router
 
 app = FastAPI()
 
-# /api/chatbot 라우터 등록
+# CORS 설정
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["http://localhost:3000/"],  # React 앱의 주소
+    allow_credentials=True,
+    allow_methods=[""],
+    allow_headers=[""],
+)
+
+/api/chatbot 라우터 등록
 app.include_router(chatbot_router, prefix="/api")
 
 @app.get("/")
 async def read_root():
     return {"message": "Hello, FastAPI!"}
-
-if __name__ == "__main__":
-    import uvicorn
-    uvicorn.run(app, host="0.0.0.0", port=8000)
